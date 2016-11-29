ca1261651a2a5c2cb161508f6386dab6344d8b79
AlbertPotter <smilepotter0128@gmail.com>
2016-11-29 17:47:58 +0800

    Remove deprication

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,6 @@ module RubyRailsSample
     config.i18n.default_locale = :ru
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
+    # config.active_record.raise_in_transactional_callbacks = true
   end
 end
